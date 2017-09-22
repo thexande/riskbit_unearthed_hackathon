f48af8c1613e9a78dace2d01b21c51121f6e33e8
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2017-09-22 00:32:50 -0600

    added controllers

---
+++ riskbit/Controllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  riskbit
+//
+//  Created by Alexander Murphy on 9/22/17.
+//  Copyright © 2017 Alexander Murphy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- riskbit/Controllers/RootViewController.swift
+++ riskbit/Controllers/RootViewController.swift
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
